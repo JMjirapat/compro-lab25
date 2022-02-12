0e9dfb34b3ab609e1468171bd463cecbb50d5204
JMjirapat <jm_jirapat@hotmail.com>
2022-02-12 15:57:26 +0700

    lab25 - All Complete

--- lab25.cpp
+++ lab25.cpp
@@ -2,6 +2,8 @@
 #include<cmath>
 using namespace std;
 
+#define PI 3.14159265
+
 class ComplexNumber{				
 	public:
 		double real;
@@ -26,6 +28,71 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
+}
+
+ComplexNumber operator+(double num, const ComplexNumber &c){
+	return ComplexNumber(num+c.real,c.imag);
+}
+
+ComplexNumber operator-(double num, const ComplexNumber &c){
+	return ComplexNumber(num-c.real,-c.imag);
+}
+
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.real == 0 && c.imag == 0)
+		return os << 0;
+	else if(c.real == 0)
+		return os << c.imag << "i";
+	else if(c.imag == 0)
+			return os << c.real;
+	else{
+		if(c.imag < 0)
+			return os << c.real << c.imag << "i";
+		else
+			return os << c.real << "+" << c.imag << "i";
+	}
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	double result_r = (real*c.real)-(imag*c.imag);
+	double result_i = (real*c.imag)+(imag*c.real);
+	return ComplexNumber(result_r,result_i);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double result_r = ((real*c.real)+(imag*c.imag))/((pow(c.real,2)+pow(c.imag,2)));
+	double result_i = ((imag*c.real)-(real*c.imag))/((pow(c.real,2)+pow(c.imag,2)));
+	return ComplexNumber(result_r,result_i);
+}
+
+ComplexNumber operator*(double num, const ComplexNumber &c){
+	ComplexNumber cn(num);
+	return cn*c;
+}
+
+ComplexNumber operator/(double num, const ComplexNumber &c){
+	ComplexNumber cn(num);
+	return cn/c;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag)
+		return true;
+	return false;
+}
+
+bool operator==(double num, const ComplexNumber &c){
+	if(num == c.real && num == c.imag)
+		return true;
+	return false;
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real) * 180 / PI;;
 }
 
 //Write your code here
